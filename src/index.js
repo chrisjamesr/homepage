07a1acaacd34a08a6b9328268938ac067de123b9
chrisjamesr <chrisjames.r@gmail.com>
2018-10-16 12:48:42 -0400

    change stylesheet extension

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
-import './styles/style.sass'
+import './styles/App.scss'
 
 ReactDOM.render(
   <App />,
-  document.getElementById('app')
+  document.getElementById('page')
 )
