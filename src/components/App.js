f765e6dc74025eed1e5932098b0ad36483cc3114
chrisjamesr <chrisjames.r@gmail.com>
2018-10-23 21:20:16 -0400

    change BrowserRouter to HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {HashRouter as Router, Route} from 'react-router-dom'
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
 import ColorComponent from './ColorComponent'
