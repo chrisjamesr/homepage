81052b3c2229f1e1d5d48116b07927728fe48ce0
chrisjamesr <chrisjames.r@gmail.com>
2018-10-24 19:41:10 -0400

    reinstall packages

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,6 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
 import ColorComponent from './ColorComponent'
-
-
 import "../styles/App.scss"
 
 class App extends React.Component {
