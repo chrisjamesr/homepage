ef081d598e23e15ec952b0586bc7e8dc6ff87d20
chrisjamesr <chrisjames.r@gmail.com>
2018-10-28 17:11:46 -0400

    remove file-loader

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,17 +2,21 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-          use: [
-            {
-                loader: 'url-loader'
-            },
-          ]
-      }
-    ]
-  }
+  mode: 'production'
+  // ,
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(jpeg|svg)$/i, 
+  //       use: {
+  //         loader: "file-loader",
+  //         options : {
+  //           name: '[path][name].[ext]'
+  //           // publicPath: '/dist/images'
+  //         }
+  //       }
+  //     }
+  //   ]
+  // }
 })
+
