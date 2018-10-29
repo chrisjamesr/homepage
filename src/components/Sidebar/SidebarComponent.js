282487fe44701c5484edf2ae808f161eaf5cfe57
chrisjamesr <chrisjames.r@gmail.com>
2018-10-28 20:46:45 -0400

    add target & rel attributes to social media links

--- src/components/Sidebar/SidebarComponent.js
+++ src/components/Sidebar/SidebarComponent.js
@@ -19,9 +19,9 @@ class SidebarComponent extends React.Component {
           <NavLink to="/contact">Contact</NavLink>          
         </nav>
         <footer id="social">
-          <a href="https://www.instagram.com/chrsjr/"><img alt="Instagram Icon"className="icon" src={insta}/></a>
-          <a href="https://twitter.com/chrsjr"><img alt="Twitter Icon" className="icon" src={twitter}/></a>
-          <a href="https://www.linkedin.com/in/chris-j-rudolph "><img alt="LinkedIn Icon" className="icon" src={linkedin}/></a>
+          <a href="https://www.instagram.com/chrsjr/" target="_blank" rel="noopener noreferrer"><img alt="Instagram Icon"className="icon" src={insta}/></a>
+          <a href="https://twitter.com/chrsjr"  target="_blank" rel="noopener noreferrer" ><img alt="Twitter Icon" className="icon" src={twitter}/></a>
+          <a href="https://www.linkedin.com/in/chris-j-rudolph" target="_blank" rel="noopener noreferrer" ><img alt="LinkedIn Icon" className="icon" src={linkedin}/></a>
         </footer>
       </aside>
     )
