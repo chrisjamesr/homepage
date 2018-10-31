87c291b00d21f4be3d69c03896c97de0b5bf2738
chrisjamesr <chrisjames.r@gmail.com>
2018-10-31 12:54:46 -0400

    import github svg. Preliminary component markup and styling

--- src/components/Main/Project.js
+++ src/components/Main/Project.js
@@ -1,9 +1,34 @@
 import React from 'react'
+// import github from '../../../images/icons/github.svg'
+import github from '../../../images/icons/iconmonstr-github.svg'
 
 const Project = ({project}) => {
+  const projectTileStyle = {
+      width: "90%",
+      border: "1px solid black",
+      alignSelf: "center",
+      margin: "5% auto",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "flexStart"
+    }
+  
   return (
-    <div>
-      <h3>{project.name}</h3>
+    <div className="project-tile"
+         style={projectTileStyle}
+    >
+      <h3 style={{alignSelf: "center", margin:".5em auto"}}>{project.name}</h3>
+      <div className="projectBody" style={{padding: "0 5%"}}>
+        <p>
+          <a href={project.repoUrl} target="_blank" rel="noopener noreferrer">
+            <img alt="Instagram Icon"className="icon" src={github}/>
+          </a>
+        </p>
+        
+        { !!project.url ? <p><a href={project.url}>Live Link</a></p> : null}
+        <p>{project.description}</p>
+
+      </div>
     </div>
   )
 }
