bfec5827468085c9c3f0c3baed37c37cdc0e05fa
chrisjamesr <chrisjames.r@gmail.com>
2018-11-04 12:28:21 -0500

    remove color component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
-import ColorComponent from './ColorComponent'
 import "../styles/App.scss"
 
 class App extends React.Component {
@@ -12,9 +11,6 @@ class App extends React.Component {
         <div>                
           <SidebarComponent/>
           <MainContainer/>                
-          {
-            // <ColorComponent/>
-          }
         </div>                  
       </Router>
     )
