b35b9983b6c141697ab5e6b640ef5f971a435eaa
chrisjamesr <chrisjames.r@gmail.com>
2018-11-05 12:50:59 -0500

    add id prop to ComponentName

--- src/components/Main/MainContainer.js
+++ src/components/Main/MainContainer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+// import { Route, Switch } from 'react-router-dom'
 import MainComponent from './MainComponent'
 import Home from './Home'
 import About from './About'
@@ -13,11 +13,10 @@ import {routes} from '../../lib/routes'
 import ColorComponent from '../ColorComponent'
 
 const components = {
-    about: About,
-    projects: ProjectsContainer,
-    contact:Contact,
-    colors: ColorComponent,
-    home: Home
+  home: Home,
+  about: About,
+  projects: ProjectsContainer,
+  contact:Contact    
   }
 
 class MainContainer extends React.Component{
@@ -33,31 +32,17 @@ class MainContainer extends React.Component{
 
   }  
 
-  componentName(key) {
-    const ComponentName = components[key]
-    return <ComponentName {...props}/>;
-  }
-
-  renderRoutes = () => {
+  renderComponents = () => {
     return Object.keys(components).map( (key,i) => {   
       const ComponentName = components[key]
-      return (
-        <Route 
-          key = {i}
-          path = {"/"+key} 
-          render= {(props)=> <ComponentName {...this.props}/>}
-        />
-      )  
+      return <ComponentName id={key} key ={i}/>  
     })
   }
 
   render(){
-    return(
-      <main>
-        <Route path="/" component={MainComponent}/>      
-      </main>
-    )
+    return <main>{this.renderComponents()}</main>
   }
+
 }
 
 export default MainContainer
