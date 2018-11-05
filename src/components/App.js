1b1ab211ddf4e8398ba85254187ee8451675ab17
chrisjamesr <chrisjames.r@gmail.com>
2018-11-05 12:48:15 -0500

    comment out browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+// import {BrowserRouter as Router, Route} from 'react-router-dom'
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
 import "../styles/App.scss"
 
 class App extends React.Component {
   render(){
-    return(
-      <Router basename={'/'}>       
-        <div>                
-          <SidebarComponent/>
-          <MainContainer/>                
-        </div>                  
-      </Router>
+    return (
+      <div>                
+        <SidebarComponent/>
+        <MainContainer/>                
+      </div>                 
     )
   }
 }
