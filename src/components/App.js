5a9eb6e45d163067adb2a7a30b5c0d54d170e564
chrisjamesr <chrisjames.r@gmail.com>
2018-11-05 13:28:28 -0500

    remove HEAD =====

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-<<<<<<< HEAD
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import { ScrollManager, WindowScroller, ElementScroller } from 'react-scroll-manager';
-import { createBrowserHistory as createHistory } from 'history';
-=======
-// import {BrowserRouter as Router, Route} from 'react-router-dom'
->>>>>>> ec64ba55c42e6eae5492c37640500945a55032eb
+
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
 import "../styles/App.scss"
