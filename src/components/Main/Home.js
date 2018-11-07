ee12221c8c8b130de1092bd7b8f87d8724d87b55
chrisjamesr <chrisjames.r@gmail.com>
2018-11-07 13:15:51 -0500

    modify text, add image

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -1,33 +1,18 @@
 import React from 'react'
+import selfie from '../../../images/selfie.jpg'
 
 const Home = ({id}) => {
   return(
     <div id={id}>
-      <h1>HOME</h1>
+      <img src={selfie}/>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
-        do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ac ut consequat semper viverra nam libero justo. Mollis aliquam 
-        ut porttitor leo a diam sollicitudin. Risus feugiat in ante metus 
-        dictum at tempor commodo. In eu mi bibendum neque egestas congue 
-        quisque. In dictum non consectetur a erat nam at lectus urna. Praesent 
-        semper feugiat nibh sed pulvinar proin. Nibh tellus molestie nunc non 
-        blandit massa enim. Tincidunt dui ut ornare lectus. Ac tincidunt vitae 
-        semper quis lectus nulla at volutpat diam. Massa eget egestas purus 
-        viverra accumsan in. Quisque egestas diam in arcu cursus euismod quis 
-        viverra nibh. Platea dictumst quisque sagittis purus sit. Orci nulla 
-        pellentesque dignissim enim sit amet. Ac felis donec et odio pellentesque 
-        diam volutpat commodo.
+        Full stack web developer with a passion for building things. With experience in Ruby on Rails,
+        JavaScript, and React and a background in electrical systems, and project management.  
+        I discovered web development through troubleshooting errors in automated building management, 
+        fire alarm, and security systems. I bring strong skills in project management and client relations 
+        that helps technology companies develop an idea into a fully realized, working product. I enjoy 
+        working with others to bring a vision to life.
       </p>
-      <p>
-        Dictum non consectetur a erat nam at lectus urna. Fames ac turpis egestas 
-        sed tempus urna. Gravida in fermentum et sollicitudin ac orci phasellus egestas. 
-        Quis eleifend quam adipiscing vitae proin. Quis lectus nulla at volutpat diam 
-        ut venenatis tellus in. Nibh mauris cursus mattis molestie a iaculis. Cursus in 
-        hac habitasse platea dictumst quisque sagittis. At volutpat diam ut venenatis 
-        tellus. Sagittis vitae et leo duis ut diam. At quis risus sed vulputate odio ut 
-        enim blandit volutpat.
-      </p>      
     </div>
   )
 }
