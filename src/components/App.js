e20172915edb069ab39eaf56364b0e1b0bcd42c6
chrisjamesr <chrisjames.r@gmail.com>
2018-11-07 13:15:17 -0500

    revert to import browserrouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,18 @@
 import React from 'react'
-import {Router} from 'react-router-dom'
-import { ScrollManager, WindowScroller, ElementScroller } from 'react-scroll-manager';
-import { createBrowserHistory as createHistory } from 'history';
-
+import {BrowserRouter as Router} from 'react-router-dom'
 import SidebarComponent from './Sidebar/SidebarComponent'
 import MainContainer from './Main/MainContainer'
 import "../styles/App.scss"
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.history = createHistory();
-  }
   render(){
     return(
-      <ScrollManager history={this.history}>
-        <Router history={this.history}>   
-          <div>                
-            <SidebarComponent/>
-            <MainContainer/>                
-          </div>                  
-        </Router>
-      </ScrollManager>
+      <Router>   
+        <div>                
+          <SidebarComponent/>
+          <MainContainer/>                
+        </div>                  
+      </Router>
     )
   }
 }
