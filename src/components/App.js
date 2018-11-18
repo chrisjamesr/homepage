b817f0997edf45ed7d63d7fb7d39af62ea017749
chrisjamesr <chrisjames.r@gmail.com>
2018-11-18 11:25:20 -0500

    add constructor w/state. Set viewport for context

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,42 @@
 import React from 'react'
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import {ResizeProvider} from './WindowContext'
 import SidebarComponent from './Sidebar/SidebarComponent'
-import MainContainer from './Main/MainContainer'
+import Header from './Header'
+import Footer from './Footer'
+import MainContainer from './MainContainer'
+import SlideBar from './SlideBar'
 import "../styles/App.scss"
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      viewport: 0
+    }
+  }
+
+  setViewport = () => {
+    this.setState({
+      viewPort: window.innerWidth
+    })
+  }
+  componentDidMount(){
+    this.setViewport()
+  }
+
   render(){
     return(
       <Router>   
-        <div>                
-          <SidebarComponent/>
-          <MainContainer/>                
+        <div>
+        { /*<ResizeProvider>
+            <SlideBar viewPortWidth={this.state.viewPort} />
+          </ResizeProvider>  */
+        }  
+          <Header/>
+          <MainContainer/>     
+          <Footer/>
+
         </div>                  
       </Router>
     )
