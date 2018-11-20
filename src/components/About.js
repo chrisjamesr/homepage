35443c8e6848eafeaf620f93356ddae03e3473f7
chrisjamesr <chrisjames.r@gmail.com>
2018-11-19 19:49:30 -0500

    add link to name

--- src/components/About.js
+++ src/components/About.js
@@ -9,8 +9,11 @@ const About = () => {
         <img src={selfie}/>
       </div>
       <p style={{textIndent: 45, lineHeight: 1.5}}>
-        My name is Chris. I'm a full-stack developer from New York City, NY. I have experience working with 
-        various front end javascript libraries like JQuery and React/Redux and frameworks like Ruby on Rails and NodeJS/Express.
+        My name is <a href="https://www.linkedin.com/in/chris-j-rudolph"
+          target="_blank" rel="noopener noreferrer" className="link">Chris Rudolph</a>. 
+          I'm a full-stack developer from New York City. I have experience working with 
+        various front end javascript libraries like JQuery and React/Redux and frameworks like Ruby 
+        on Rails and NodeJS/Express.
 
       </p>
       {/*<Social border/>*/}
